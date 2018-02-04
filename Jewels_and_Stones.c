2f01a0242c08d9c4964dc423c3dd29cf1dedaae4
Amit <solapurkar.amit@gmail.com>
2018-02-03 23:04:03 -0800

    771. Jewels and Stones Leetcode question

---
+++ Jewels_and_Stones.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int numJewelsInStones(char* J, char* S) {
+    
+    int i, j;
+    int count=0;
+    
+    for(i=0;J[i]!='\0';i++)
+    {
+        for(j=0;S[j]!='\0';j++)
+        {
+            if(J[i]==S[j])
+            {count++;}
+        }
+        
+    }
+    
+    printf("%d",count);
+    return count;
+}
